e4bd85fd8a5e368e2672184192adee2f7be3f40a
johnnyRobot <beitschalbua@gmx.at>
2023-10-19 14:24:30 +0200

    form trial

---
+++ car-park-frontend/src/Pages/Layout.jsx
@@ -0,0 +1,9 @@
+import { Outlet, Link } from "react-router-dom";
+const Layout = () => {
+    return (<div>
+    <h2>Hallo</h2>
+        <Outlet/>
+    </div>)
+}
+
+export default Layout;
--- car-park-frontend/src/main.jsx
+++ car-park-frontend/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Layout from "./Pages/Layout.jsx";
+import ErrorPage from "./Pages/ErrorPage.jsx";
+//import CarCreator from "./Pages/CarCreator.jsx"
+import CarForm from "./Components/CarForm.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <CarForm />,
+            }
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
