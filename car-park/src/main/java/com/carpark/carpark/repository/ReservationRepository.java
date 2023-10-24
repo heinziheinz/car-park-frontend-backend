20aa07b12ab351c68ad7f33cb32c91e66ace9922
johnnyRobot <beitschalbua@gmx.at>
2023-10-24 09:11:58 +0200

    minor change

--- car-park/src/main/java/com/carpark/carpark/repository/ReservationRepository.java
+++ car-park/src/main/java/com/carpark/carpark/repository/ReservationRepository.java
@@ -1,7 +1,11 @@
 package com.carpark.carpark.repository;
 
+import com.carpark.carpark.model.Car;
 import com.carpark.carpark.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
 }
