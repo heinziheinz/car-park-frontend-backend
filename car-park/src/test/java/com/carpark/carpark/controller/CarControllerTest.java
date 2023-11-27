94e85d27d5841d4f1417328a190fa3048dc6bcc9
johnnyRobot <beitschalbua@gmx.at>
2023-11-27 14:43:43 +0100

    all tests added to endpoint of CarController

--- car-park/src/test/java/com/carpark/carpark/controller/CarControllerTest.java
+++ car-park/src/test/java/com/carpark/carpark/controller/CarControllerTest.java
@@ -15,11 +15,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.LocalDate;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 //@SpringBootTest
 @WebMvcTest(CarController.class)//the fastes Context
@@ -83,6 +84,72 @@ class CarControllerTest {
         mockMvc.perform(get(url + "/id/" + id).accept(APPLICATION_JSON).contentType(APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
         Mockito.verify(carReservationService).findCarById(id);
+    }
+
+    @Test
+    void update() throws Exception {
+        long id = 1;
+        String json = "{\"id\": \"1\", \"typeName\": \"Honda\", \"price\": \"200\", \"image\": \"image\"}";
+        Car car = new Car(1, "Honda", 200, "image");
+
+        mockMvc.perform(put(url + "/" + id).accept(APPLICATION_JSON).contentType(APPLICATION_JSON).content(json))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        Mockito.verify(carReservationService).updateCar(id, car);
+    }
+
+    @Test
+    void rentACar() throws Exception {
+        long carId = 1;
+        long userId = 1;
+        LocalDate startDate = LocalDate.of(2022, 10, 22);
+        LocalDate endDate = LocalDate.of(2022, 10, 22);
+        mockMvc.perform(post(url + "/" + carId + "/user/" + userId + "/" + startDate + "/" + endDate).accept(APPLICATION_JSON).contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        Mockito.verify(carReservationService).rentACar(carId, userId, startDate, endDate);
+    }
+
+    @Test
+    void getAllAvailableCars() throws Exception {
+        LocalDate startDate = LocalDate.of(2022, 10, 22);
+        LocalDate endDate = LocalDate.of(2022, 10, 22);
+        mockMvc.perform(get(url + "/" + "find-available-cars-for-rent/" + startDate + "/" + endDate).accept(APPLICATION_JSON).contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        Mockito.verify(carReservationService).findAvailableCars(startDate, endDate);
+    }
+
+    @Test
+    void getAllAvailableCarsByName() throws Exception {
+        String carHouseName = "SuperCar";
+        LocalDate startDate = LocalDate.of(2022, 10, 22);
+        LocalDate endDate = LocalDate.of(2022, 10, 22);
+        mockMvc.perform(get(url + "/" + "find-available-cars-for-rent-by-name/" + carHouseName + "/" + startDate + "/" + endDate).accept(APPLICATION_JSON).contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        Mockito.verify(carReservationService).getAvailableCarsByCarHouseName(carHouseName, startDate, endDate);
+    }
+
+    @Test
+    void getCarWitchIsInSpecificCarHouse() throws Exception {
+        long carHouseId = 1;
+        mockMvc.perform(get(url + "/find-all-cars-with-in-carhouse/" + carHouseId).accept(APPLICATION_JSON).contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        Mockito.verify(carReservationService).findAllCarsInCarHouse(ArgumentMatchers.eq(carHouseId), ArgumentMatchers.any(Pageable.class));
+    }
+
+    @Test
+    void getAllCArsNotAllocatedToCarHouse() throws Exception{
+        mockMvc.perform(get(url + "/all-cars-not-allocated-to-a-carhouse").accept(APPLICATION_JSON).contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        Mockito.verify(carReservationService).findAllCArsNotAllocatedToACarHouse(ArgumentMatchers.any(Pageable.class));
+    }
+
+    @Test
+    void transferCarToCarPool() throws Exception{
+        long carhouseId = 1;
+        long carId = 1;
+        long carPoolId = 1;
+        mockMvc.perform(post(url + "/"+ carhouseId + "/transfer-car-to-car-pool/" + carId).accept(APPLICATION_JSON).contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        Mockito.verify(carReservationService).transferCarFromCarHouseToCarPool(carhouseId, carId, carPoolId);
     }
 
 }
