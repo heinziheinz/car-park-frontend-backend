19cdc881f0c8dbecef3581e4dd9698a0e8754f7f
johnnyRobot <beitschalbua@gmx.at>
2023-12-06 18:15:41 +0100

    LogOut front end fetch cookie delete

--- car-park-frontend/src/Pages/LogOut.jsx
+++ car-park-frontend/src/Pages/LogOut.jsx
@@ -1,24 +1,41 @@
 import {LogginInContext} from "../main.jsx";
 import {UserRoleContext} from "../main.jsx";
-import {useEffect} from "react";
-import {useContext} from "react";
+import {useEffect, useContext} from "react";
+import {useNavigate} from "react-router-dom";
+import {fetchAuthenticated} from "../Utilities/api.js";
 
 const LogOut = () => {
-    const {loggedIn, setLoggedIn} = useContext(LogginInContext);
-    const {userRole, setUserRole} = useContext(UserRoleContext);
+    const {setLoggedIn} = useContext(LogginInContext);
+    const {setUserRole} = useContext(UserRoleContext);
+    const navigate = useNavigate();
     useEffect(() => {
-        setLoggedIn(false);
-        setUserRole("USER");
-        localStorage.clear();
+        (async () => {
+            try {
+                const loggedOut = await fetchAuthenticated(`/user-logout`, {
+                    method: "GET"
+                });
+                if (loggedOut.ok) {
+                    setLoggedIn(false);
+                    setUserRole("USER");
+                    localStorage.clear();
+                    navigate("/login")
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        })();
+
 
     }, []);
 
     return (
         <div>
             <img height="auto" width="800px" src={"https://imgur.com/7MNlyvp.png"}/>
-            <h3 style={{"position":"absolute",
-            "top":"300px", "left":"100px", "fontStyle": "italic", "color":"red", "fontSize":"100px"
-            , "textDecoration": "underline"}}>Danke</h3>
+            <h3 style={{
+                "position": "absolute",
+                "top": "300px", "left": "100px", "fontStyle": "italic", "color": "red", "fontSize": "100px"
+                , "textDecoration": "underline"
+            }}>Danke</h3>
         </div>
     );
 }
